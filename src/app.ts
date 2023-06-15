21bbc7685ec46416a105eb4aa62cfcaa5e259a88
janeluz <janerodrigues_2610@hotmail.com>
2023-06-15 14:15:30 -0300

    feat: add config docker and prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const  app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name:'Jane Rodrigues',
+        email:'janeteste@gmail.com'
+
+    },
+})
